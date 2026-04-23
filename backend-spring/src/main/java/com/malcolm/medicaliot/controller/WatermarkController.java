bbb3fb11ded95ade95ed647cec0017951dcb6b14
Malcolm Cephas <137897091+malcolm-cephas@users.noreply.github.com>
2026-04-23 08:00:47 +0530

    Merge branch

--- backend-spring/src/main/java/com/malcolm/medicaliot/controller/WatermarkController.java
+++ backend-spring/src/main/java/com/malcolm/medicaliot/controller/WatermarkController.java
@@ -7,7 +7,6 @@ import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import java.util.Map;
-import java.util.Objects;
 
 /**
  * WatermarkController
